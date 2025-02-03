5038525f7d5f97da88c8dc3c9eb240dc738730ba
kyranxx <blanarikdaniel@gmail.com>
2025-02-03 20:07:34 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { loadStripe } from '@stripe/stripe-js'
 
 // Initialize Stripe
-export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!)
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
